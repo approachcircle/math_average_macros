ca7cc0c075e229321950ba2e90b95d7d5d62e58a
approachcircle <58401347+approachcircle@users.noreply.github.com>
2021-10-17 21:00:10 +0100

    delete main.rs
    dont really need an example, the only need the file containing the module and macros

--- src/main.rs
+++
@@ -1,8 +0,0 @@
-// implementation
-mod average;
-
-fn main() {
-    println!("{}", get_median!(vec!(1,2,3,5,3,2,1)));
-    println!("{}", get_range!(vec!(100, 200, 300, 400, 500)));
-    println!("{}", get_mean!(vec!(4, 4, 56, 2, 5, 3, 21)));
-}
